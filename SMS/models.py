f237fcef825e183ff620bb037dc3d14b02f99b9c
raofaisalmaqbool <raofaisalmaqbool@gmail.com>
2022-03-16 14:11:49 +0500

    make tables

--- SMS/models.py
+++ SMS/models.py
@@ -1,3 +1,22 @@
+from email.headerregistry import Address
 from django.db import models
 
 # Create your models here.
+class Students(models.Model):
+    ID = models.IntegerField(primary_key=True)
+    Name = models.CharField(max_length=50)
+    Father_Name= models.CharField(max_length=50)
+    Address=models.CharField(max_length=80)
+    Phone_No= models.CharField(max_length=11)
+
+
+class Teachers(models.Model):
+    ID = models.IntegerField(primary_key=True)
+    Name = models.CharField(max_length=50)
+    Address= models.CharField(max_length=50)
+    Email=models.EmailField(max_length=54)
+    Phone_No= models.CharField(max_length=11)
+
+class Class(models.Model):
+    ID = models.IntegerField(primary_key=True)
+    
