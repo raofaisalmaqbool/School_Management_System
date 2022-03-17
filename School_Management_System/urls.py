93fc7c4cfad8c10174607fe03861e5f9c4260a4f
raofaisalmaqbool <raofaisalmaqbool@gmail.com>
2022-03-17 10:40:41 +0500

    create views files and directories in templates

--- School_Management_System/urls.py
+++ School_Management_System/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from SMS import views
 from django.conf import settings
 from django.conf.urls.static import static
+from.import principal_views, teachers_views, students_views
 
 
 urlpatterns = [
