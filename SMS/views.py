6621f116f749c5ee3b9ffc22f39fa4941f16ba7a
raofaisalmaqbool <raofaisalmaqbool@gmail.com>
2022-03-20 23:52:43 +0500

    set profile backend page

--- SMS/views.py
+++ SMS/views.py
@@ -3,6 +3,7 @@ from SMS.emailbackend import EmailBackend
 from django.contrib.auth import authenticate, login as logino, logout
 from django.contrib import messages
 from SMS.models import Customuser
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -38,9 +39,40 @@ def doLogin(request):
            messages.error(request,'Email or Password are Invalid !')
            return redirect('login')
 
+@login_required(login_url='/')
 def profile(request):
     user = Customuser.objects.get(id = request.user.id)
-    context ={
-        "user" : user
+
+
+    context = {
+        "user":user,
     }
-    return render(request, 'profile.html', context)
+    return render(request,'profile.html',context)
+
+@login_required(login_url='/')
+def profile_update(request):
+    if request.method == "POST":
+        profile_pic = request.FILES.get('profile_pic')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        #email = request.POST.get('email')
+        #username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(profile_pic)
+        try:
+            customuser = Customuser.objects.get(id = request.user.id)
+
+            customuser.first_name = first_name
+            customuser.last_name = last_name
+
+            if password !=None and password != "":
+                customuser.set_password(password)
+            if profile_pic !=None and profile_pic != "":
+                customuser.profile_pic = profile_pic
+            customuser.save()
+            messages.success(request,'Your Profile Updated Successfully !')
+            return redirect('profile')
+        except:
+            messages.error(request,'Failed To Update Your Profile')
+
+    return render(request,'profile.html')
