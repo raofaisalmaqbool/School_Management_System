2442b5391287b8c9065ed0c717782295ccb54987
raofaisalmaqbool <raofaisalmaqbool@gmail.com>
2022-03-21 12:53:19 +0500

    create functins in prinipal_views of student to save students data and informtions

--- School_Management_System/principal_views.py
+++ School_Management_System/principal_views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from SMS.models import Course, Session_Year 
+from SMS.models import Course, Customuser, Session_Year, Student 
+from django.contrib import messages
 
 @login_required(login_url='/')
 def principal_home(request):
@@ -11,9 +12,52 @@ def add_student(request):
     course = Course.objects.all()
     session_year = Session_Year.objects.all()
 
-    context = {
-        'course' : course,
-        'session_year' : session_year,
+    if request.method == "POST":
+        profile_pic = request.FILES.get('profile_pic')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        address = request.POST.get('address')
+        gender = request.POST.get('gender')
+        course_id = request.POST.get('course_id')
+        session_year_id = request.POST.get('session_year_id')
 
+        if Customuser.objects.filter(email=email).exists():
+           messages.warning(request,'Email Is Already Taken')
+           return redirect('add_student')
+        if Customuser.objects.filter(username=username).exists():
+           messages.warning(request,'Username Is Already Taken')
+           return redirect('add_student')
+        else:
+            user = Customuser(
+                first_name = first_name,
+                last_name = last_name,
+                username = username,
+                email = email,
+                profile_pic = profile_pic,
+                user_type = 3
+            )
+            user.set_password(password)
+            user.save()
+
+            course = Course.objects.get(id=course_id)
+            session_year = Session_Year.objects.get(id=session_year_id)
+
+            student = Student(
+                admin = user,
+                address = address,
+                session_year_id = session_year,
+                course_id = course,
+                gender = gender,
+            )
+            student.save()
+            messages.success(request, user.first_name + "  " + user.last_name + " student are Successfully Added !")
+            return redirect('add_student')
+
+    context = {
+        'course':course,
+        'session_year':session_year,
     }
     return render(request,'principal/add_student.html', context)
