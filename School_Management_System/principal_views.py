89d76d918bd9a560d4c74a79ef103beb52a02d23
raofaisalmaqbool <raofaisalmaqbool@gmail.com>
2022-03-21 16:40:02 +0500

    student update setting from backend, by id

--- School_Management_System/principal_views.py
+++ School_Management_System/principal_views.py
@@ -88,3 +88,51 @@ def edit_student(request,id):
         'session_year':session_year,
     }
     return render(request,'principal/edit_student.html',context)
+
+
+@login_required(login_url='/')
+def update_student(request):
+    if request.method == "POST":
+        student_id = request.POST.get('student_id')
+        print(student_id)
+        profile_pic = request.FILES.get('profile_pic')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        address = request.POST.get('address')
+        gender = request.POST.get('gender')
+        course_id = request.POST.get('course_id')
+        session_year_id = request.POST.get('session_year_id')
+
+        user = Customuser.objects.get(id = student_id)
+
+        user.first_name = first_name
+        user.last_name = last_name
+        user.email = email
+        user.username = username
+
+        if password != None and password != "":
+            user.set_password(password)
+        if profile_pic != None and profile_pic != "":
+            user.profile_pic = profile_pic
+        user.save()
+
+        student = Student.objects.get(admin = student_id)
+        student.address = address
+        student.gender = gender
+
+        course = Course.objects.get(id = course_id)
+        student.course_id = course
+
+        session_year = Session_Year.objects.get(id = session_year_id)
+        student.session_year_id = session_year
+
+        student.save()
+        messages.success(request,'Student Record Are Successfully Updated !')
+        return redirect('view_student')
+
+    return render(request,'principal/edit_student.html')
+#34 update student (Backend).txt
+ #34 update student (Backend).txt.
