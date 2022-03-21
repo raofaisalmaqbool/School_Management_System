0bd7558bb4c922eac9bca00b59397669bb45fb37
raofaisalmaqbool <raofaisalmaqbool@gmail.com>
2022-03-21 11:00:28 +0500

    Create Models of Course and Session Year and register them

--- SMS/admin.py
+++ SMS/admin.py
@@ -7,3 +7,5 @@ class UserModel(UserAdmin):
     list_display = ['username','user_type']
 
 admin.site.register(Customuser, UserAdmin)
+admin.site.register(Course)
+admin.site.register(Session_Year)
--- SMS/models.py
+++ SMS/models.py
@@ -1,6 +1,7 @@
 from distutils.command.upload import upload
 from email.headerregistry import Address
 import profile
+from turtle import update
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import User
@@ -35,5 +36,20 @@ class Customuser(AbstractUser):
 
     user_type = models.CharField(choices=USER, max_length=50, default=1)
     profile_pic = models.ImageField(upload_to='media/profile_pic')
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=80)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self): # this will return name either the course object 1,2,3
+        return self.name
+    
+class Session_Year(models.Model):
+    session_start = models.CharField(max_length=80)
+    session_end = models.CharField(max_length=80)
+
+    
 
     
