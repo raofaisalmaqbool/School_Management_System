10138e8957374808ed74e00ce692eeea4cd4da39
raofaisalmaqbool <raofaisalmaqbool@gmail.com>
2022-03-22 18:24:15 +0500

    set backend of add teacher,

--- School_Management_System/principal_views.py
+++ School_Management_System/principal_views.py
@@ -2,7 +2,7 @@ from email import message
 from http.client import HTTPResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from SMS.models import Course, Customuser, Session_Year, Student 
+from SMS.models import Course, Customuser, Session_Year, Student, Teacher 
 from django.contrib import messages
 from SMS.models import Student
 
@@ -202,4 +202,42 @@ def delete_course(request, id):
 
 
 def add_teacher(request):
+    if request.method == "POST":     # it will get the post request
+        profile_pic = request.FILES.get('profile_pic')  # this will get the data from fom and save it
+        first_name = request.POST.get('first_name')     # in the variable 
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        address = request.POST.get('address')
+        gender = request.POST.get('gender')
+
+        if Customuser.objects.filter(email=email).exists():
+           messages.warning(request,'Email Is Already Taken')
+           return redirect('add_teacher')
+        if Customuser.objects.filter(username=username).exists():
+           messages.warning(request,'Username Is Already Taken')
+           return redirect('add_teacher')
+        
+        else:
+            user = Customuser(
+                first_name = first_name,
+                last_name = last_name,
+                username = username,
+                email = email,
+                profile_pic = profile_pic,
+                user_type = 2
+            )
+            user.set_password(password)     # password is equal to password nahi likh sakty is liya 
+            user.save()
+
+            teacher = Teacher(
+                admin = user,
+                address = address,
+                gender = gender,
+            )
+            teacher.save()
+            messages.success(request, user.first_name + "  " + user.last_name + " Teacher is Successfully Added !")
+            return redirect('add_teacher')
+
     return render(request, "principal/add_teacher.html")
