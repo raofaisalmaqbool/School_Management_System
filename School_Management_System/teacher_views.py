1b0e6576699cfbce27a5b521dbc1ecab41b254c4
raofaisalmaqbool <raofaisalmaqbool@gmail.com>
2022-03-25 17:38:48 +0500

    update

--- School_Management_System/teacher_views.py
+++ School_Management_System/teacher_views.py
@@ -5,4 +5,19 @@ def teacher_home(request):
     return render(request, 'teacher/teacher_home.html')
 
 def notifications_tec(request):
-    return render(request, 'teacher/teacher_notification.html')
+    teacher = Teacher.objects.filter(admin = request.user.id)
+    for i in teacher:
+        teacher_id =i.id
+        notifications_tec = Teacher_Notification.objects.filter(teacher_id = teacher_id)
+        context ={
+            'notifications_tec' : notifications_tec
+        }
+        return render(request, 'teacher/teacher_notification.html', context)
+
+def status_mark(request, status):
+    notification = Teacher_Notification.objects.get(id = status)
+    notification.status = 1
+    notification.save()
+    return redirect('notifications_tec')
+
+
