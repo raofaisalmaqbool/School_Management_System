1b9a43f30637618f33257e62f01f6c160da42f4d
raofaisalmaqbool <raofaisalmaqbool@gmail.com>
2022-03-31 15:35:45 +0500

    set the all form dynamic by createing individual file and functions

--- SMS/singleton_class.py
+++ SMS/singleton_class.py
@@ -1,4 +1,7 @@
+from ast import IsNot
+import email
 from django.db import models
+import re
 
 
 # class Form_Validation():
@@ -6,8 +9,10 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 from urllib import request
 
+from SMS.models import Customuser
 
-def validations(profile_pic, first_name):
+
+def validations(profile_pic, first_name, last_name, email, username):
     d = {}
     # validation for empty first_name
     # num = 0
@@ -19,11 +24,78 @@ def validations(profile_pic, first_name):
         # num = num+1
 
     # validation for correct enter first_name
-    if not first_name.isalpha() or first_name != "":
+    if not first_name.isalpha() and first_name != "":
         # messages.warning(request, 'please correct enter first name')
         # return redirect('add_student')
         # num = num+1
         d['e2'] = "please enter correct first name"
         # d['num'] = d['num'] + 1
+
+    # validation for empty last_name
+    if not last_name:
+        # messages.warning(request, 'please enter first name')
+        # return redirect('add_student')
+        d['e3'] = "please enter last name"
+        # d['num'] = d['num'] + 1
+        # num = num+1
+
+    # validation for correct enter last_name
+    if not last_name.isalpha() and last_name != "":
+        # messages.warning(request, 'please correct enter first name')
+        # return redirect('add_student')
+        # num = num+1
+        d['e4'] = "please enter correct last name"
+        # d['num'] = d['num'] + 1
+
+    regex = '''!#$%&'() *+,-"/:;<=>?[\]^_`{|}~'''
+    abc = str(email)
+    for i in abc:
+        if i in regex:
+            d['e5'] = "invalid email"
+
+    if not username.isalnum() or username == "":
+        d['e6'] = "something wrong with username please use alphabet or numeric numbers"
+
+    pic_name = str(profile_pic)
+    exa = ".png"
+    exb = ".jpg"
+
+    # ab =""
+    # print(pic_name)
+    # for i in pic_name[-4::1]:
+    #     ab = ab.append(i)
+    if exa in pic_name or exb in pic_name:
+        pass
+    else:
+        d['e7'] = "invalid image not PNG pic"
+
+
+
+    # if regex not in email:
+    #     d['e5'] = "invalid email"
+
+
+# Define a function for
+# for validating an Email
+
+    # def check(email):
+
+        # # pass the regular expression
+        # # and the string into the fullmatch() method
+        #     if(re.fullmatch(regex, email)):
+        # 	    pass
+
+        #     else:
+
+    # check(email)
+
+    # if Customuser.objects.filter(email=email).exists():
+    #     d['e6'] = "Email Is Already Taken"
+        # messages.warning(request, 'Email Is Already Taken')
+
+    # email1 = email.split("@")[0]
+    # email2 = email.split("@")[-1]
+    # if not email1.isalnum():
+    #     d['e5'] = "only google gmail is allowed"
 
     return d
--- School_Management_System/principal_views.py
+++ School_Management_System/principal_views.py
@@ -3,24 +3,27 @@ from http.client import HTTPResponse
 from urllib import request
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from SMS.models import Course, Customuser, Session_Year, Student, Student_Leave, Teacher, Teacher_Notification,Teacher_leave
+from SMS.models import Course, Customuser, Session_Year, Student, Student_Leave, Teacher, Teacher_Notification, Teacher_leave
 from django.contrib import messages
 from SMS.models import Student
 from SMS.singleton_class import validations
-  
+
+
 @login_required(login_url='/')
 def principal_home(request):
-    teacher_count = Teacher.objects.all().count() # they will return total no of teachre, studnt ,courses
+    # they will return total no of teachre, studnt ,courses
+    teacher_count = Teacher.objects.all().count()
     student_count = Student.objects.all().count()
     course_count = Course.objects.all().count()
 
-    context ={
-        'teacher_count' : teacher_count,
-        'student_count' : student_count,
-        'course_count' : course_count
+    context = {
+        'teacher_count': teacher_count,
+        'student_count': student_count,
+        'course_count': course_count
     }
 
     return render(request, 'principal/principal_home.html', context)
+
 
 @login_required(login_url='/')
 def add_student(request):
@@ -29,7 +32,6 @@ def add_student(request):
 
     if request.method == "POST":
         profile_pic = request.FILES.get('profile_pic')
-        print(profile_pic)
         first_name = request.POST.get('first_name')
         last_name = request.POST.get('last_name')
         email = request.POST.get('email')
@@ -42,19 +44,19 @@ def add_student(request):
 
         # p_pic = profile_pic.split(".")[-1]
         # if p_pic == "zip":
-            # mgk
-        #validation for empty first_name
+        # mgk
+        # validation for empty first_name
         x = 0
-        d = validations(profile_pic, first_name)
+        d = validations(profile_pic, first_name, last_name, email, username)
         for i in d.values():
-            messages.warning(request,i)
-            x=x+1
-        
-        if x!=0:
+            messages.warning(request, i)
+            x = x+1
+
+        if x != 0:
             return redirect('add_student')
 
         # print(n)
-        
+
         # num = 0
         # if not first_name:
         #     messages.warning(request,'please enter first name')
@@ -66,7 +68,7 @@ def add_student(request):
         #     messages.warning(request,'please correct enter first name')
         #     #return redirect('add_student')
         #     num = num+1
-        
+
         # #validation for empty last_name
         # if not last_name:
         #     messages.warning(request,'please enter last name')
@@ -81,14 +83,14 @@ def add_student(request):
 
         # # validation for unique email
         if Customuser.objects.filter(email=email).exists():
-           messages.warning(request,'Email Is Already Taken')
-           #return redirect('add_student')
+            messages.warning(request, 'Email Is Already Taken')
+            return redirect('add_student')
         #    num = num+1
 
         # # validation for unique username
         if Customuser.objects.filter(username=username).exists():
-           messages.warning(request,'Username Is Already Taken')
-           #return redirect('add_student')
+            messages.warning(request, 'Username Is Already Taken')
+            # return redirect('add_student')
         #    num = num+1
 
         # if num != 0:
@@ -96,12 +98,12 @@ def add_student(request):
 
         else:
             user = Customuser(
-                first_name = first_name,
-                last_name = last_name,
-                username = username,
-                email = email,
-                profile_pic = profile_pic,
-                user_type = 3
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+                email=email,
+                profile_pic=profile_pic,
+                user_type=3
             )
             user.set_password(password)
             user.save()
@@ -110,47 +112,51 @@ def add_student(request):
             session_year = Session_Year.objects.get(id=session_year_id)
 
             student = Student(
-                admin = user,
-                address = address,
-                session_year_id = session_year,
-                course_id = course,
-                gender = gender,
+                admin=user,
+                address=address,
+                session_year_id=session_year,
+                course_id=course,
+                gender=gender,
             )
             student.save()
-            messages.success(request, user.first_name + "  " + user.last_name + " student are Successfully Added !")
+            messages.success(request, user.first_name + "  " +
+                             user.last_name + " student are Successfully Added !")
             return redirect('add_student')
 
     context = {
-        'course':course,
-        'session_year':session_year,
+        'course': course,
+        'session_year': session_year,
     }
-    return render(request,'principal/add_student.html', context)
+    return render(request, 'principal/add_student.html', context)
+
 
 @login_required(login_url='/')
 def view_student(request):
-    return render(request,'principal/view_student.html')
+    return render(request, 'principal/view_student.html')
+
 
 @login_required(login_url='/')
 def view_student(request):
     student = Student.objects.all()
 
     context = {
-        'student':student,
+        'student': student,
     }
-    return render(request,'principal/view_student.html',context)
+    return render(request, 'principal/view_student.html', context)
+
 
 @login_required(login_url='/')
-def edit_student(request,id):
-    student = Student.objects.filter(id = id)
+def edit_student(request, id):
+    student = Student.objects.filter(id=id)
     course = Course.objects.all()
     session_year = Session_Year.objects.all()
 
     context = {
-        'student':student,
-        'course':course,
-        'session_year':session_year,
+        'student': student,
+        'course': course,
+        'session_year': session_year,
     }
-    return render(request,'principal/edit_student.html',context)
+    return render(request, 'principal/edit_student.html', context)
 
 
 @login_required(login_url='/')
@@ -169,7 +175,7 @@ def update_student(request):
         course_id = request.POST.get('course_id')
         session_year_id = request.POST.get('session_year_id')
 
-        user = Customuser.objects.get(id = student_id)
+        user = Customuser.objects.get(id=student_id)
 
         user.first_name = first_name
         user.last_name = last_name
@@ -182,29 +188,30 @@ def update_student(request):
             user.profile_pic = profile_pic
         user.save()
 
-        student = Student.objects.get(admin = student_id)
+        student = Student.objects.get(admin=student_id)
         student.address = address
         student.gender = gender
 
-        course = Course.objects.get(id = course_id)
+        course = Course.objects.get(id=course_id)
         student.course_id = course
 
-        session_year = Session_Year.objects.get(id = session_year_id)
+        session_year = Session_Year.objects.get(id=session_year_id)
         student.session_year_id = session_year
 
         student.save()
-        messages.success(request,'Student Record Are Successfully Updated !')
+        messages.success(request, 'Student Record Are Successfully Updated !')
         return redirect('view_student')
 
-    return render(request,'principal/edit_student.html')
-#34 update student (Backend).txt
- #34 update student (Backend).txt.
+    return render(request, 'principal/edit_student.html')
+# 34 update student (Backend).txt
+ # 34 update student (Backend).txt.
+
 
 @login_required(login_url='/')
-def delete_student(request,admin):
-    student = Customuser.objects.get(id = admin)
+def delete_student(request, admin):
+    student = Customuser.objects.get(id=admin)
     student.delete()
-    messages.success(request,'Record Are Successfully Deleted !')
+    messages.success(request, 'Record Are Successfully Deleted !')
     return redirect('view_student')
 
 
@@ -215,39 +222,43 @@ def add_course(request):
         course_name = request.POST.get('course_name')
 
         course = Course(
-            name = course_name,
+            name=course_name,
         )
         course.save()
-        messages.success(request,'Course Are Successfully Created ')
+        messages.success(request, 'Course Are Successfully Created ')
 
-        
         return redirect('add_course')
 
-    return render(request,'principal/add_course.html')
+    return render(request, 'principal/add_course.html')
 
 
 @login_required(login_url='/')
 def view_course(request):
     course = Course.objects.all()
     context = {
-        'course':course,
+        'course': course,
     }
-    return render(request,'principal/view_course.html',context)
+    return render(request, 'principal/view_course.html', context)
+
 
 @login_required(login_url='/')
 def edit_course(request, id):
     course = Course.objects.get(id=id)
     context = {
-        'course' : course
+        'course': course
     }
     return render(request, 'principal/edit_course.html', context)
 
+
 def update_course(request):
     if request.method == "POST":
-        name = request.POST.get('name')# form ky ander course name ko bhaja tha name ky title sy osko yahan per get kiya ha
-        course_id = request.POST.get('course_id')# or course id bhaji thi edit_course ma wo get krwai
+        # form ky ander course name ko bhaja tha name ky title sy osko yahan per get kiya ha
+        name = request.POST.get('name')
+        # or course id bhaji thi edit_course ma wo get krwai
+        course_id = request.POST.get('course_id')
 
-        course = Course.objects.get(id = course_id) #models ma say course object ki id mil jay gi issy
+        # models ma say course object ki id mil jay gi issy
+        course = Course.objects.get(id=course_id)
         course.name = name  # for course name change
         course.save()
         messages.success(request, 'Course Updated Successfully!')
@@ -255,17 +266,19 @@ def update_course(request):
 
     return render(request, 'principal/edit_course.html')
 
+
 def delete_course(request, id):
     course = Course.objects.get(id=id)    # ya model ki id laye ga
     course.delete()        # will delete the course
-    messages.success(request,'Course is deleted!')
+    messages.success(request, 'Course is deleted!')
     return redirect('view_course')
 
 
 def add_teacher(request):
     if request.method == "POST":     # it will get the post request
-        profile_pic = request.FILES.get('profile_pic')  # this will get the data from fom and save it
-        first_name = request.POST.get('first_name')     # in the variable 
+        # this will get the data from fom and save it
+        profile_pic = request.FILES.get('profile_pic')
+        first_name = request.POST.get('first_name')     # in the variable
         last_name = request.POST.get('last_name')
         email = request.POST.get('email')
         username = request.POST.get('username')
@@ -274,66 +287,69 @@ def add_teacher(request):
         gender = request.POST.get('gender')
 
         if Customuser.objects.filter(email=email).exists():
-           messages.warning(request,'Email Is Already Taken')
-           return redirect('add_teacher')
-           
+            messages.warning(request, 'Email Is Already Taken')
+            return redirect('add_teacher')
+
         if Customuser.objects.filter(username=username).exists():
-           messages.warning(request,'Username Is Already Taken')
-           return redirect('add_teacher')
-        
+            messages.warning(request, 'Username Is Already Taken')
+            return redirect('add_teacher')
+
         else:
             user = Customuser(
-                first_name = first_name,
-                last_name = last_name,
-                username = username,
-                email = email,
-                profile_pic = profile_pic,
-                user_type = 2
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+                email=email,
+                profile_pic=profile_pic,
+                user_type=2
             )
-            user.set_password(password)     # password is equal to password nahi likh sakty is liya 
+            # password is equal to password nahi likh sakty is liya
+            user.set_password(password)
             user.save()
 
             teacher = Teacher(
-                admin = user,
-                address = address,
-                gender = gender,
+                admin=user,
+                address=address,
+                gender=gender,
             )
             teacher.save()
-            messages.success(request, user.first_name + "  " + user.last_name + " Teacher is Successfully Added !")
+            messages.success(request, user.first_name + "  " +
+                             user.last_name + " Teacher is Successfully Added !")
             return redirect('add_teacher')
 
     return render(request, "principal/add_teacher.html")
 
+
 def view_teacher(request):
     teacher = Teacher.objects.all()
-    context ={
-        'teacher' : teacher
+    context = {
+        'teacher': teacher
     }
     return render(request, 'principal/view_teacher.html', context)
 
 
 def edit_teacher(request, id):
     teacher = Teacher.objects.get(id=id)
-    context ={
-        'teacher':teacher
+    context = {
+        'teacher': teacher
     }
     return render(request, "principal/edit_teacher.html", context)
 
+
 def delete_teacher(request, admin):
-    teacher = Customuser.objects.get(id = admin)
+    teacher = Customuser.objects.get(id=admin)
     teacher.delete()
     messages.success(request, 'Record Successfully!')
-    #return render(request, 'principal/view_teacher.html')
+    # return render(request, 'principal/view_teacher.html')
     return redirect('view_teacher')
-
 
 
 def teacher_send_notifiction(request):
     teacher = Teacher.objects.all()
     see_notification = Teacher_Notification.objects.all().order_by('-id')
-    context ={
-        'teacher' : teacher,
-        'see_notification' : see_notification,
+    context = {
+        'teacher': teacher,
+        'see_notification': see_notification,
     }
     return render(request, 'principal/teacher_notification.html', context)
 
@@ -345,10 +361,10 @@ def save_teacher_notification(request):
 
         #print(teacher_id, message)
 
-        teacher = Teacher.objects.get(admin = teacher_id)
+        teacher = Teacher.objects.get(admin=teacher_id)
         notifcation = Teacher_Notification(
-            teacher_id = teacher,
-            message = message,
+            teacher_id=teacher,
+            message=message,
 
         )
         notifcation.save()
@@ -356,24 +372,23 @@ def save_teacher_notification(request):
     return redirect('teacher_send_notifiction')
 
 
-def teacher_leave_view(request): 
+def teacher_leave_view(request):
     teacher_leave = Teacher_leave.objects.all()
-    context ={
-        'teacher_leave' : teacher_leave
+    context = {
+        'teacher_leave': teacher_leave
     }
     return render(request, 'principal/teacher_leave.html', context)
 
 
-
 def teacher_leave_approve(request, id):
-    leave = Teacher_leave.objects.get(id = id)
+    leave = Teacher_leave.objects.get(id=id)
     leave.status = 1
     leave.save()
     return redirect('teacher_leave_view')
-    
+
 
 def teacher_leave_disapprove(requesst, id):
-    leave = Teacher_leave.objects.get(id = id)
+    leave = Teacher_leave.objects.get(id=id)
     leave.status = 2
     leave.save()
     return redirect('teacher_leave_view')
@@ -382,20 +397,20 @@ def teacher_leave_disapprove(requesst, id):
 def student_leave_view(request):
     student_leave = Student_Leave.objects.all()
     context = {
-        'student_leave' : student_leave
+        'student_leave': student_leave
     }
     return render(request, 'principal/student_leave.html', context)
 
 
 def student_leave_approve(request, id):
-    leave = Student_Leave.objects.get(id = id)
+    leave = Student_Leave.objects.get(id=id)
     leave.status = 1
     leave.save()
     return redirect('student_leave_view')
-    
+
 
 def student_leave_disapprove(requesst, id):
-    leave = Student_Leave.objects.get(id = id)
+    leave = Student_Leave.objects.get(id=id)
     leave.status = 2
     leave.save()
     return redirect('student_leave_view')
