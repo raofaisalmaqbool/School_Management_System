446d9d72d0081d2db618b978703fb001f16f836b
raofaisalmaqbool <raofaisalmaqbool@gmail.com>
2022-04-01 18:18:25 +0500

    project connected with mysql and superuser is created

--- School_Management_System/settings.py
+++ School_Management_System/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'School_Management_System.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'lms_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
